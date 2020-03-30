8c275999f2b615f0eb6ec887e313e280c3312241
pmaigutyak <pmaigutyak@gmail.com>
2020-03-30 22:46:27 +0300

    Add js manager.

--- cart/templatetags/cart.py
+++ cart/templatetags/cart.py
@@ -7,6 +7,14 @@ from cart.lib import get_cart
 register = template.Library()
 
 
+@register.inclusion_tag(
+    'cart/cart-js.html',
+    takes_context=True,
+    name='cart_js')
+def render_cart_js(context):
+    return context
+
+
 @register.simple_tag(name='get_cart', takes_context=True)
 def get_cart_tag(context):
     return get_cart(context['request'])
